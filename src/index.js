2e6ddb9e8115d75d0d477bdc684928dff355019c
Paul Morgan III <pmorgan3@gmail.com>
2019-09-13 11:00:00 -0500

    Decided to let vscode format for me

--- src/index.js
+++ src/index.js
@@ -1,79 +1,106 @@
-const tedious = require('tedious');
-const EventEmitter = require('events');
-const config = require('./config').config
-const knex_config = require('./config').knex
-const Sql = require('knex')(knex_config)
+const tedious = require("tedious");
+const EventEmitter = require("events");
+const config = require("./config").config;
+const knex_config = require("./config").knex;
+const Sql = require("knex")(knex_config);
 
 class KeyvMssql extends EventEmitter {
-
   constructor(opts) {
     super(opts);
     this.ttlSupport = false;
-    this.opts = Object.assign({
-      table: 'keyv',
-      keySize: 255
-    }, opts);
+    this.opts = Object.assign(
+      {
+        table: "keyv",
+        keySize: 255
+      },
+      opts
+    );
 
-    Sql.schema.hasTable(this.opts.table)
-      .then(this.keyvtable = Sql(this.opts.table),
-        () => {
-          Sql.schema.createTable(this.opts.table, table => {
-            table.string('key').primary().notNullable().unique().index();
-            table.text('value').nullable().defaultTo(null);
-          });
-          this.keyvtable = Sql(this.opts.table);
+    Sql.schema
+      .hasTable(this.opts.table)
+      .then((this.keyvtable = Sql(this.opts.table)), () => {
+        Sql.schema.createTable(this.opts.table, table => {
+          table
+            .string("key")
+            .primary()
+            .notNullable()
+            .unique()
+            .index();
+          table
+            .text("value")
+            .nullable()
+            .defaultTo(null);
         });
-
+        this.keyvtable = Sql(this.opts.table);
+      });
   }
 
   async get(key) {
     const client = Sql(this.opts.table);
-    const row = await client.select('value').where({
-      'key': key
-    }).returning('value').then(p => p[0]).catch(TypeError);
+    const row = await client
+      .select("value")
+      .where({
+        key: key
+      })
+      .returning("value")
+      .then(p => p[0])
+      .catch(TypeError);
 
     if (row === undefined) {
       return undefined;
     }
 
-    return row['value'];
+    return row["value"];
   }
 
   async set(key, value) {
-
-    value = value.replace('"', '\"').replace(/\\/g, '\\\\').replace(/[(')+]/g, "''").replace(/[\0]+/g, '');
+    value = value
+      .replace('"', '"')
+      .replace(/\\/g, "\\\\")
+      .replace(/[(')+]/g, "''")
+      .replace(/[\0]+/g, "");
 
     const client = Sql(this.opts.table);
     let setResult = Promise.resolve(undefined);
     let insertSucceeded = false;
     //try {
-    await client.insert({
-      'key': key,
-      'value': value
-    }).catch(tedious.RequestError).then(async () => await client.update({
-      'key': key,
-      'value': value
-    }));
+    await client
+      .insert({
+        key: key,
+        value: value
+      })
+      .catch(tedious.RequestError)
+      .then(
+        async () =>
+          await client.update({
+            key: key,
+            value: value
+          })
+      );
 
-    console.log('insert succeeded for', key, value);
+    console.log("insert succeeded for", key, value);
     insertSucceeded = true;
 
     return insertSucceeded;
   }
 
   async delete(key) {
-    let doesKeyExist = await this.get(key)
-    if (doesKeyExist === undefined)
-      return false;
+    let doesKeyExist = await this.get(key);
+    if (doesKeyExist === undefined) return false;
     const client = Sql(this.opts.table);
-    const exists = await client.where({
-      'key': key
-    }).select('*');
-    console.log('exists', exists);
+    const exists = await client
+      .where({
+        key: key
+      })
+      .select("*");
+    console.log("exists", exists);
     if (exists) {
-      return await client.where({
-        'key': key
-      }).del().then(() => true, () => false);
+      return await client
+        .where({
+          key: key
+        })
+        .del()
+        .then(() => true, () => false);
     }
     return false;
   }
@@ -82,11 +109,11 @@ class KeyvMssql extends EventEmitter {
     const client = Sql(this.opts.table);
     try {
       return await client.del().then(() => {
-        return undefined
+        return undefined;
       });
     } catch (error) {
-      console.log('clear failed', error)
-      return undefined
+      console.log("clear failed", error);
+      return undefined;
     }
   }
 }
